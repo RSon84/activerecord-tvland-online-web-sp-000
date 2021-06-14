debed50663a203400b716e28fecf565c2fd4275b
Devin Wilson <craigvin84@yahoo.com>
2021-06-14 21:57:22 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   def actors_list
-    self.characters.actors.collect do |character|
+    self.characters.collect do |character|
     "#{full_name}"
   end
 end

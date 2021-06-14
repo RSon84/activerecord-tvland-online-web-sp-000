7bc5729771db5bb4da87b5b9b7bd5870734b1f59
Devin Wilson <craigvin84@yahoo.com>
2021-06-14 21:53:20 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,8 +3,8 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   def actors_list
-    characters.collect do |character|
-    "#{character.full_name}"
+    actors.collect do |character|
+    "#{actors.full_name}"
   end.join
 end
 end

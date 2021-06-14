50228be2bf8ff8c5073771fce4f00d7f6ec9a030
Devin Wilson <craigvin84@yahoo.com>
2021-06-14 21:51:02 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
   belongs_to :network
   has_many :characters
-  has_many :actors
+  has_many :actors, through: :characters
   def actors_list
     self.collect do |a|
     "#{Actor.full_name}"
